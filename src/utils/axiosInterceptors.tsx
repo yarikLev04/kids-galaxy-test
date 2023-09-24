f324dd22edf0210648b82604e8a13e6175205554
yarikLev04 <91241935+yarikLev04@users.noreply.github.com>
2023-09-24 17:39:15 +0200

    fix axios

--- src/utils/axiosInterceptors.tsx
+++ src/utils/axiosInterceptors.tsx
@@ -1,12 +1,16 @@
 import API from '@/config';
 import axios from 'axios';
+import * as https from 'https';
 import { useSession } from 'next-auth/react';
 
 const axiosInstance = axios.create({
   baseURL: API.baseUrl,
   headers: {
     'Content-type': 'application/json; charset=UTF-8'
-  }
+  },
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
 });
 
 export const useAxiosInterceptors = () => {
